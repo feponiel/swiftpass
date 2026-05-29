4aa928eb494820e87e305d5ea2c7da3f518af6c8
Felipe Elias <felipedaniel.me@gmail.com>
2026-05-29 07:54:34 -0300

    test: add unit tests for CreateTicketUseCase

---
+++ src/test/java/com/feponiel/swiftpass/unit/usecases/CreateTicketTest.java
@@ -0,0 +1,103 @@
+package com.feponiel.swiftpass.unit.usecases;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.feponiel.swiftpass.domain.application.repositories.EventsRepository;
+import com.feponiel.swiftpass.domain.application.repositories.TicketsRepository;
+import com.feponiel.swiftpass.domain.application.usecases.CreateTicketUseCase;
+import com.feponiel.swiftpass.domain.application.usecases.exceptions.EventNotFoundException;
+import com.feponiel.swiftpass.domain.application.usecases.exceptions.InvalidCurrencyException;
+import com.feponiel.swiftpass.domain.business.entities.Event;
+import com.feponiel.swiftpass.domain.business.entities.Ticket;
+import com.feponiel.swiftpass.factories.EventFactory;
+import com.feponiel.swiftpass.factories.TicketFactory;
+
+public class CreateTicketTest {
+  @Mock private TicketsRepository ticketsRepository;
+  @Mock private EventsRepository eventsRepository;
+
+  @InjectMocks
+  private CreateTicketUseCase createTicketUseCase;
+
+  private AutoCloseable mocks;
+
+  @BeforeEach
+  void setup() {
+    mocks = MockitoAnnotations.openMocks(this);
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
+    mocks.close();
+  }
+
+  @Test
+  void shouldCreateTicket() {
+    Event event = EventFactory.make();
+    Ticket ticketTemplate = TicketFactory.make(t -> t.eventId(event.getId()));
+
+    when(eventsRepository.findById(event.getId())).thenReturn(Optional.of(event));
+
+    Ticket newTicket = createTicketUseCase.execute(
+      ticketTemplate.getEventId(),
+      ticketTemplate.getName(),
+      ticketTemplate.getDescription(),
+      ticketTemplate.getPrice(),
+      ticketTemplate.getCurrency(),
+      ticketTemplate.getCapacity()
+    );
+
+    verify(ticketsRepository, times(1)).create(newTicket);
+  }
+
+  @Test
+  void shouldNotCreateTicketWhenEventDoesNotExists() {
+    Ticket ticket = TicketFactory.make();
+
+    when(eventsRepository.findById(ticket.getEventId())).thenReturn(Optional.empty());
+
+    assertThrows(EventNotFoundException.class, () -> createTicketUseCase.execute(
+      ticket.getEventId(),
+      ticket.getName(),
+      ticket.getDescription(),
+      ticket.getPrice(),
+      ticket.getCurrency(),
+      ticket.getCapacity()
+    ));
+
+    verify(ticketsRepository, never()).create(any());
+  }
+
+  @Test
+  void shouldNotCreateTicketWhenCurrencyIsInvalid() {
+    Event event = EventFactory.make();
+    Ticket ticketWithInvalidCurrency = TicketFactory.make(t -> t.eventId(event.getId()).currency("ZZZ"));
+
+    when(eventsRepository.findById(event.getId())).thenReturn(Optional.of(event));
+
+    assertThrows(InvalidCurrencyException.class, () -> createTicketUseCase.execute(
+      ticketWithInvalidCurrency.getEventId(),
+      ticketWithInvalidCurrency.getName(),
+      ticketWithInvalidCurrency.getDescription(),
+      ticketWithInvalidCurrency.getPrice(),
+      ticketWithInvalidCurrency.getCurrency(),
+      ticketWithInvalidCurrency.getCapacity()
+    ));
+
+    verify(ticketsRepository, never()).create(any());
+  }
+}
