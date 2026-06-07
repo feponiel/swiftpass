837fc9f256981e8a2ff84e96d164a81a3d687cee
Felipe Elias <felipedaniel.me@gmail.com>
2026-06-07 16:51:55 -0300

    fix: add patch mapping annotation in promote/demote organizer controllers

--- src/main/java/com/feponiel/swiftpass/infrastructure/http/controllers/PromoteUserToOrganizer.java
+++ src/main/java/com/feponiel/swiftpass/infrastructure/http/controllers/PromoteUserToOrganizer.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 public class PromoteUserToOrganizer {
   private PromoteUserToOrganizerUseCase promoteUserToOrganizerUseCase;
 
+  @PatchMapping
   public ResponseEntity<Void> handle(@PathVariable UUID userId) {
     this.promoteUserToOrganizerUseCase.execute(userId);
 
