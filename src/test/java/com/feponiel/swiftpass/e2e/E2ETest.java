3fac82200fbaf243a40c52488f765bfdd5a84687
Felipe Elias <felipedaniel.me@gmail.com>
2026-06-07 16:35:38 -0300

    test: add base class for e2e tests

---
+++ src/test/java/com/feponiel/swiftpass/e2e/E2ETest.java
@@ -0,0 +1,35 @@
+package com.feponiel.swiftpass.e2e;
+
+import org.junit.jupiter.api.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Tag("e2e")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Testcontainers
+public abstract class E2ETest {
+  @Container
+  @SuppressWarnings("resource")
+  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+    .withDatabaseName("swiftpass_test")
+    .withUsername("test")
+    .withPassword("test");
+
+  @DynamicPropertySource
+  static void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgres::getJdbcUrl);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasource.password", postgres::getPassword);
+  }
+
+  @Autowired
+  protected MockMvc mockMvc;
+}
