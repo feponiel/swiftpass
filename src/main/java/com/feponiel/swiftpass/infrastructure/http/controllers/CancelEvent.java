2876be34bdab54e27b6fa13e3069ba2c92ea2f0c
Felipe Elias <felipedaniel.me@gmail.com>
2026-06-07 21:07:36 -0300

    fix: restrict CancelEvent Controller to organizers

--- src/main/java/com/feponiel/swiftpass/infrastructure/http/controllers/CancelEvent.java
+++ src/main/java/com/feponiel/swiftpass/infrastructure/http/controllers/CancelEvent.java
@@ -3,6 +3,7 @@ package com.feponiel.swiftpass.infrastructure.http.controllers;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.feponiel.swiftpass.domain.application.usecases.CancelEventUseCase;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@Secured("ROLE_ORGANIZER")
 @RequestMapping("/events/{eventId}/cancel")
 @RequiredArgsConstructor
 public class CancelEvent {
